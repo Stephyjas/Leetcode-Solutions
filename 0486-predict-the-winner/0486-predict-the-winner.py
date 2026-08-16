0ff1200437f1b10ba3ccdfeea51d1866a4ef40e3
Jashlin Stephy PS <jashlinstephy2006@gmail.com>
2026-08-16 19:57:32 +0530

    Time: 3455 ms (15.29%), Space: 12.5 MB (45.55%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,12 @@
+class Solution:
+    def predictTheWinner(self, nums):
+        def solve(left, right):
+            if left == right:
+                return nums[left]
+
+            take_left = nums[left] - solve(left + 1, right)
+            take_right = nums[right] - solve(left, right - 1)
+
+            return max(take_left, take_right)
+
+        return solve(0, len(nums) - 1) >= 0
